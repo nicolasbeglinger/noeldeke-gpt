8bbc4355ae2aae19da6f339d3275c210015db836
Nicolas Beglinger <beglinger.nicolas@gmail.com>
2024-06-05 13:01:57 +0200

    Format code

--- chat/views.py
+++ chat/views.py
@@ -14,17 +14,17 @@ import locale
 
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
+
 def chat(request):
     response_message = ""
     user_input = ""
 
-    thread_id = request.session.get('thread_id')
+    thread_id = request.session.get("thread_id")
 
     if request.method == "POST":
-        user_input = request.POST.get('user_input')
+        user_input = request.POST.get("user_input")
         print("user_input: ", user_input)
         date_string = f"Heutiges Datum: {datetime.datetime.now().strftime('%A, %d. %B %Y, %H:%M')}"
-
 
         # Create an Assistant
         noeldekegpt = client.beta.assistants.retrieve("asst_aXieuBEQXmx4yovAGO7VYzxL")
@@ -33,7 +33,7 @@ def chat(request):
             # Create a new thread if none exists
             my_thread = client.beta.threads.create()
             thread_id = my_thread.id
-            request.session['thread_id'] = thread_id
+            request.session["thread_id"] = thread_id
         else:
             # Use the existing thread
             my_thread = client.beta.threads.retrieve(thread_id=thread_id)
@@ -54,8 +54,7 @@ def chat(request):
         # Check the status of the run
         while my_run.status in ["queued", "in_progress"]:
             my_run = client.beta.threads.runs.retrieve(
-                thread_id=my_thread.id,
-                run_id=my_run.id
+                thread_id=my_thread.id, run_id=my_run.id
             )
             time.sleep(0.5)  # A brief pause to prevent rapid polling
 
@@ -64,35 +63,43 @@ def chat(request):
                 all_messages = client.beta.threads.messages.list(thread_id=my_thread.id)
                 response_message = all_messages.data[0].content[0].text.value
                 # Get rid of square brackets (Quellen)
-                response_message = re.sub(r'\【.*?\】', '', response_message)
+                response_message = re.sub(r"\【.*?\】", "", response_message)
                 # Markdown -> HTML
                 response_message = markdown2.markdown(response_message)
-                
+
                 # Save the QnA record
                 qna_record = QnA(question=user_input, answer=response_message)
                 qna_record.save()
 
-                print(f'{response_message = }')
+                print(f"{response_message = }")
                 break
 
-        return JsonResponse({'message': response_message})
+        return JsonResponse({"message": response_message})
 
     # Retrieve all past QnA records
-    past_questions = QnA.objects.all().order_by('-timestamp')
-    return render(request, 'chat.html', {
-        'user_input': user_input,
-        'response': response_message,
-        'past_questions': past_questions,
-    })
+    past_questions = QnA.objects.all().order_by("-timestamp")
+    return render(
+        request,
+        "chat.html",
+        {
+            "user_input": user_input,
+            "response": response_message,
+            "past_questions": past_questions,
+        },
+    )
+
 
 def tipps_tricks(request):
-    return render(request, 'tipps_tricks.html')
+    return render(request, "tipps_tricks.html")
+
 
 def milchkalender(request):
     # Set the locale to German
-    locale.setlocale(locale.LC_TIME, 'de_DE')
+    locale.setlocale(locale.LC_TIME, "de_DE")
 
-    with open(os.path.join(settings.BASE_DIR, "chat", "data", "milch_daten_2024.txt"), "r") as f:
+    with open(
+        os.path.join(settings.BASE_DIR, "chat", "data", "milch_daten_2024.txt"), "r"
+    ) as f:
         data = f.readlines()[1:]
         data = [d.strip() for d in data]
 
@@ -105,4 +112,8 @@ def milchkalender(request):
 
     current_date = datetime.datetime.now().strftime("%A, %d. %B %Y")
 
-    return render(request, 'milchkalender.html', {'future_milk_dates': future_milk_dates[:5], 'current_date': current_date})
+    return render(
+        request,
+        "milchkalender.html",
+        {"future_milk_dates": future_milk_dates[:5], "current_date": current_date},
+    )
