ca6c6e7486a08e7e567ef9801fe1b3208dcf9837
Nicolas Beglinger <beglinger.nicolas@gmail.com>
2024-08-09 14:24:13 +0200

    updated cors_allowed_origins

--- noeldekegpt/settings.py
+++ noeldekegpt/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://[::]:8000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://beg.ch"
+    "https://www.beg.ch"
 ]
 
 ROOT_URLCONF = "noeldekegpt.urls"
